4af7815d8c8afefd55db4cf05bd44d7adf483228
Lucas Santos <lucasparaipaba113@gmail.com>
2020-07-31 22:18:08 -0300

    Output layout improved

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const functions = require('./functions/allFunctions.js')
 
 function main() {
   let inputs = getInputsUser()
-  
+
   let optionOfCalc = inputs.optionOfCalc
   let num1 = inputs.num1
   let num2 = inputs.num2
-
+  
   let resu = functions[optionOfCalc](num1,num2)
-  console.log(resu)
+  resu = parseFloat(resu.toFixed(2))
+
+  console.clear()
+  console.log(
+    '\n=======================' +
+    '\n Op: ' + optionOfCalc +
+    '\n Numero 1: ' + num1 +
+    '\n Numero 2: ' + num2 +
+    '\n Resultado: ' + resu +
+    '\n=======================' 
+  )
 }
 
 while(true) 
